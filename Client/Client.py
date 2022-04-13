2fef0d23ee3a38a2604619221c9525334d207aa3
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2022-04-13 00:15:02 -0300

    minor changes

--- Client/Client.py
+++ Client/Client.py
@@ -4,7 +4,7 @@ from socket import socket, AF_INET, SOCK_STREAM
 class Client:
     def send_random_payload(self, sock: socket) -> None:
         with open("random.txt", "r") as file:
-            # map to string with format initial code and n separated by space
+            # map initial code and n as string separated by space
             payloads = [f"{line.split()[0]} {line.split()[1]}" for line in file.readlines()]
 
         for payload in payloads:
